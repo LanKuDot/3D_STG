900c133d5a42e5fa92250e6aeb4b5e733e9d4abe
LanKuDot <airlanser@gmail.com>
2020-09-15 11:58:57 +0800

    Make ObjectPool be static and manage many items

--- Assets/Scripts/Common/ObjectPool.cs
+++ Assets/Scripts/Common/ObjectPool.cs
@@ -1,24 +1,56 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections.Generic;
 
 public class ObjectPool : MonoBehaviour
 {
-    public GameObject targetObject;
+    public static ObjectPool Instance;
 
-    private readonly Queue<GameObject> _pool = new Queue<GameObject>();
+    public ObjectPoolItem[] poolItems;
 
-    public GameObject GetObject()
+    private readonly Dictionary<string, Queue<GameObject>> _pools =
+        new Dictionary<string, Queue<GameObject>>();
+    private readonly Dictionary<string, GameObject> _objectsToPool =
+        new Dictionary<string, GameObject>();
+
+    private void Awake()
     {
-        var obj =
-            _pool.Count == 0 ? Instantiate(targetObject) : _pool.Dequeue();
-        obj.SetActive(true);
+        Instance = this;
+
+        foreach (var item in poolItems) {
+            var queue = new Queue<GameObject>();
+            _pools.Add(item.name, queue);
+
+            for (var i = 0; i < item.initialNum; ++i) {
+                var obj = Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                obj.transform.SetParent(transform);
+                queue.Enqueue(obj);
+            }
+
+            _objectsToPool.Add(item.name, item.objectToPool);
+        }
+    }
+
+    public GameObject GetObject(string objName)
+    {
+        var pool = _pools[objName];
+        var obj = pool.Count == 0 ?
+            Instantiate(_objectsToPool[objName]) : pool.Dequeue();
         return obj;
     }
 
-    public void ReturnObject(GameObject obj)
+    public void ReturnObject(string objName, GameObject obj)
     {
-        obj.SetActive(false);
         obj.transform.SetParent(transform);
-        _pool.Enqueue(obj);
+        _pools[objName].Enqueue(obj);
     }
+}
+
+[Serializable]
+public class ObjectPoolItem
+{
+    public string name;
+    public GameObject objectToPool;
+    public int initialNum;
 }
