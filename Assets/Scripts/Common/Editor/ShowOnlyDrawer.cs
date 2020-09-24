c08f3a34add0cd2a8f72fc0fb7dd0214a77240e0
LanKuDot <airlanser@gmail.com>
2020-09-24 16:00:16 +0800

    Add ShowOnly attribute

---
+++ Assets/Scripts/Common/Editor/ShowOnlyDrawer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ShowOnlyAttribute))]
+public class ShowOnlyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        using (new EditorGUI.DisabledGroupScope(true))
+            EditorGUI.PropertyField(position, property, label, true);
+    }
+}
+
+[CustomPropertyDrawer(typeof(BeginShowOnlyGroupAttribute))]
+public class BeginReadOnlyGroupDrawer : DecoratorDrawer
+{
+    public override float GetHeight()
+    {
+        return 0;
+    }
+
+    public override void OnGUI(Rect position)
+    {
+        EditorGUI.BeginDisabledGroup(true);
+    }
+}
+
+[CustomPropertyDrawer(typeof(EndShowOnlyGroupAttribute))]
+public class EndReadOnlyGroupDrawer : DecoratorDrawer
+{
+    public override float GetHeight()
+    {
+        return 0;
+    }
+
+    public override void OnGUI(Rect position)
+    {
+        EditorGUI.EndDisabledGroup();
+    }
+}
---
+++ Assets/Scripts/Common/ShowOnlyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The attribute for marking a field readonly in the inspector
+/// </summary>
+public class ShowOnlyAttribute : PropertyAttribute
+{ }
+
+/// <summary>
+/// The attribute for beginning a readonly field group
+/// </summary>
+public class BeginShowOnlyGroupAttribute : PropertyAttribute
+{ }
+
+/// <summary>
+/// The attribute for ending a readonly field group
+/// </summary>
+public class EndShowOnlyGroupAttribute : PropertyAttribute
+{ }
