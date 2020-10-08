5d1450f1daa3219e69e16e3a0d7fda9063444cda
LanKuDot <airlanser@gmail.com>
2020-10-08 16:39:08 +0800

    Add EnemyRotate

---
+++ Assets/Scripts/GamePlay/EnemyRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace GamePlay
+{
+    public class EnemyRotate : Enemy
+    {
+        [SerializeField]
+        private EnemyRotateData _data = null;
+
+        private new void OnEnable()
+        {
+            hp = _data.hp;
+            base.OnEnable();
+        }
+
+        private void FixedUpdate()
+        {
+            Look(_data.rotatingVelocity * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/GamePlay/EnemyRotateData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GamePlay
+{
+    [CreateAssetMenu(fileName = "EnemyRotateData",
+        menuName = "Scriptable Object/Enemy/Enemy Rotate Data", order = 3)]
+    public class EnemyRotateData : ScriptableObject
+    {
+        [SerializeField]
+        private int _hp = 3;
+        [SerializeField]
+        private float _rotatingVelocity = 120f;
+
+        public int hp => _hp;
+        public float rotatingVelocity => _rotatingVelocity;
+    }
+}
