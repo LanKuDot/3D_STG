1f2b75b49d07e4ea429677473b850cff643bb343
LanKuDot <airlanser@gmail.com>
2020-10-16 10:45:07 +0800

    WakeTrigger: Set the pass through direction from local up to local forward

--- Assets/Scripts/GamePlay/WakeTrigger.cs
+++ Assets/Scripts/GamePlay/WakeTrigger.cs
@@ -5,18 +5,29 @@ namespace GamePlay
     /// <summary>
     /// The trigger that when the player passes it, it will wake up the registered objects
     /// </summary>
+    [RequireComponent(typeof(BoxCollider))]
     public class WakeTrigger : MonoBehaviour
     {
+        [SerializeField] [ShowOnly]
+        private BoxCollider _boxCollider = null;
         [SerializeField]
         private GameObject[] _targetObjects = null;
+
+        public BoxCollider boxCollider => _boxCollider;
+
+        private void Reset()
+        {
+            _boxCollider = GetComponent<BoxCollider>();
+            _boxCollider.isTrigger = true;
+        }
 
         private void OnTriggerExit(Collider other)
         {
             if (!other.CompareTag("Player"))
                 return;
 
-            // If the player goes through its local up direction, treat as passed.
-            var towardDirection = transform.rotation * Vector3.up;
+            // If the player goes through its local forward direction, treat as passed.
+            var towardDirection = transform.forward;
             var objectDirection = other.transform.position - transform.position;
             if (Vector3.Dot(towardDirection, objectDirection) > 0)
                 WakeUpTargets();
