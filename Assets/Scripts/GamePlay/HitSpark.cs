c034aa3a45c4f36ce3ffcf343e05fe166c8eaef1
LanKuDot <airlanser@gmail.com>
2020-11-30 15:02:47 +0800

    Add hit spark

---
+++ Assets/Scripts/GamePlay/HitSpark.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace GamePlay
+{
+    public class HitSpark : MonoBehaviour
+    {
+        public void OnParticleSystemStopped()
+        {
+            ReturnToPool();
+        }
+
+        /// <summary>
+        /// Return itself back to the object pool
+        /// </summary>
+        private void ReturnToPool()
+        {
+            transform.SetParent(ObjectPool.Instance.gameObject.transform);
+            ObjectPool.Instance.ReturnObject(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/GamePlay/PlayerBullet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace GamePlay
+{
+    /// <summary>
+    /// The bullet shot from the player
+    /// </summary>
+    public class PlayerBullet : Bullet
+    {
+        private new void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Trigger"))
+                return;
+
+            if (!other.CompareTag("Bullet"))
+                SpawnSpark();
+
+            base.OnTriggerEnter(other);
+        }
+
+        /// <summary>
+        /// Spawn the spark at the tip of the bullet
+        /// </summary>
+        private void SpawnSpark()
+        {
+            var spark = ObjectPool.Instance.GetObject("HitSpark");
+            var sparkTransform = spark.transform;
+            var upDirection = transform.up;
+
+            sparkTransform.position = transform.position + upDirection * 0.25f;
+            sparkTransform.forward = upDirection * -1;
+            sparkTransform.parent = null;
+            spark.SetActive(true);
+        }
+    }
+}
