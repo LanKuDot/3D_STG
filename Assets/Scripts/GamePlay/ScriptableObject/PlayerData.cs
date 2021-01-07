1ca64578c8fbfdaf4e65ce28264f4268952df877
LanKuDot <airlanser@gmail.com>
2021-01-07 14:13:33 +0800

    Narrow the accessibilty of PlayerData

--- Assets/Scripts/GamePlay/ScriptableObject/PlayerData.cs
+++ Assets/Scripts/GamePlay/ScriptableObject/PlayerData.cs
@@ -8,11 +8,24 @@ namespace GamePlay
         order = 1)]
     public class PlayerData : ScriptableObject
     {
-        public int hp = 3;
-        public float movingVelocity = 10.0f;
-        public float movingAccelTime = 0.1f;
-        public float rotatingAccelTime = 0.1f;
-        public GameObject bullet;
-        public float firingInterval = 0.5f;
+        [SerializeField]
+        private int _hp = 3;
+        [SerializeField]
+        private float _movingVelocity = 10.0f;
+        [SerializeField]
+        private float _movingAccelTime = 0.1f;
+        [SerializeField]
+        private float _rotatingAccelTime = 0.1f;
+        [SerializeField]
+        private GameObject _bullet;
+        [SerializeField]
+        private float _firingInterval = 0.15f;
+
+        public int hp => _hp;
+        public float movingVelocity => _movingVelocity;
+        public float movingAccelTime => _movingAccelTime;
+        public float rotatingAccelTime => _rotatingAccelTime;
+        public GameObject bullet => _bullet;
+        public float firingInterval => _firingInterval;
     }
 }
