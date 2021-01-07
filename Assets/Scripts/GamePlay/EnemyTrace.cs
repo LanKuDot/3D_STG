a02ebe946f03dce06274836c3c0ab1e11ffb5581
LanKuDot <airlanser@gmail.com>
2021-01-07 17:40:40 +0800

    EnemyTrace: Do shock-back action when hit by the protector

--- Assets/Scripts/GamePlay/EnemyTrace.cs
+++ Assets/Scripts/GamePlay/EnemyTrace.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace GamePlay
 {
@@ -9,6 +10,10 @@ namespace GamePlay
 
         private Transform _playerTarget;
         private SmoothMove _smoothMove;
+
+        private bool _shockBack = false;
+        private const float _shockBackTime = 0.08f;
+        private const float _shockBackVelocity = 0.9f;
 
         private void Awake()
         {
@@ -28,6 +33,24 @@ namespace GamePlay
             TracePlayer();
         }
 
+        private new void OnTriggerEnter(Collider other)
+        {
+            base.OnTriggerEnter(other);
+
+            // When the enemy trace hit by the player's protector,
+            // do the shock back action.
+            if (other.CompareTag("Protector")) {
+                _shockBack = true;
+                StartCoroutine(ShockBackCountDown());
+            }
+        }
+
+        private IEnumerator ShockBackCountDown()
+        {
+            yield return new WaitForSeconds(_shockBackTime);
+            _shockBack = false;
+        }
+
         private void TracePlayer()
         {
             var towardVector = _playerTarget.position - transform.position;
@@ -37,9 +60,12 @@ namespace GamePlay
             var towardDeg =
                 Vector3.SignedAngle(Vector3.forward, towardVector, Vector3.up);
 
+            // If it has to do shock back action, it moves far away from the player
             var deltaDistance =
-                _smoothMove.MoveDelta(
-                    new Vector2(towardVector.x, towardVector.z), Time.deltaTime);
+                _shockBack ?
+                    new Vector2(-towardVector.x, -towardVector.z) * _shockBackVelocity :
+                    _smoothMove.MoveDelta(
+                        new Vector2(towardVector.x, towardVector.z), Time.deltaTime);
             var deltaDeg =
                 _smoothMove.RotateDelta(transform.eulerAngles.y,
                     towardDeg, Time.deltaTime);
